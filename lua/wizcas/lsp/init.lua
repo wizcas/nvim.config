6be2b24f740afb5f3133ec3dc7475bfef500321a
Zhuo Chen <zhuo.chen@compass.com>
2022-06-08 21:44:15 +0800

    lsp settings

--- lua/wizcas/lsp/init.lua
+++ lua/wizcas/lsp/init.lua
@@ -3,6 +3,6 @@ if not status_ok then
   return
 end
 
-require "user.lsp.configs"
-require("user.lsp.handlers").setup()
-require "user.lsp.null-ls"
+require "wizcas.lsp.configs"
+require("wizcas.lsp.handlers").setup()
+require "wizcas.lsp.null-ls"
