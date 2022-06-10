b0e6b3c6db626024e22fd695ae42f4d114d41e7d
Zhuo Chen <zhuo.chen@compass.com>
2022-06-10 19:20:58 +0800

    guifont

--- lua/wizcas/options.lua
+++ lua/wizcas/options.lua
@@ -33,7 +33,7 @@ local options = {
   wrap = false, -- display lines as one long line
   scrolloff = 8, -- is one of my fav
   sidescrolloff = 8,
-  guifont = "monospace:h20", -- the font used in graphical neovim applications
+  guifont = "CaskaydiaCove Nerd Font Mono:h16", -- the font used in graphical neovim applications
   autoread = true, -- automatically reload changed files
 }
 
