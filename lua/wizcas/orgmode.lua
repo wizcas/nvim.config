c98fa42a44a29217b564ceb9d695d24df7557780
wizcas <chen@0x1c.dev>
2022-06-18 17:02:52 +0800

    org mode configurations

--- lua/wizcas/orgmode.lua
+++ lua/wizcas/orgmode.lua
@@ -6,5 +6,30 @@ end
 orgmode.setup_ts_grammar()
 orgmode.setup({
   org_agenda_files = { '~/OneDrive/notes/orgs/**/*' },
-  org_default_notes_file = '~/OneDrive/notes/org/wizcas.org'
+  org_default_notes_file = '~/OneDrive/notes/orgs/wizcas.org',
+  org_todo_keywords = { 'TODO(t)', 'WIP(w)', '|', 'DONE(d)' },
+  mappings = {
+    org = {
+      org_toggle_checkbox = '<leader>ov',
+    }
+  },
+  org_capture_templates = {
+    t = {
+      description = 'Tasks',
+      template = '* TODO %^{TODO}\n%U\n',
+      target = '~/OneDrive/notes/orgs/wizcas.org',
+    },
+    j = {
+      description = 'Journals',
+      template = '* %^{TITLE}\n%U\r\n---\n',
+      target = '~/OneDrive/notes/orgs/journals.org',
+    }
+  }
 })
+
+local ok2, headlines = pcall(require, 'headlines')
+if not ok2 then
+  return
+end
+
+headlines.setup()
--- lua/wizcas/plugins.lua
+++ lua/wizcas/plugins.lua
@@ -64,6 +64,8 @@ return packer.startup(function(use)
   use 'tmux-plugins/vim-tmux-focus-events' -- This is needed for auto reloading file changes
   use 'sindrets/diffview.nvim'
   use 'nvim-orgmode/orgmode'
+  use 'dhruvasagar/vim-table-mode'
+  use 'lukas-reineke/headlines.nvim'
 
   -- Colorschemes
   use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
