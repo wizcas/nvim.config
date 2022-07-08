610be16d529e3994735702ce250ce243636301a5
Zhuo Chen <zhuo.chen@compass.com>
2022-07-09 01:40:39 +0800

    resume telescope

--- lua/wizcas/whichkey.lua
+++ lua/wizcas/whichkey.lua
@@ -97,6 +97,7 @@ local mappings = {
   },
   ["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
   ["P"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
+  ["R"] = { "<cmd>Telescope resume<cr>", "Resume Telescope"},
 
   p = {
     name = "Packer",
