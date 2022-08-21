66408e7f6803ebd8aaedf73394cc896062a0ff40
Wizcas Chen <chen@0x1c.dev>
2022-08-22 00:30:13 +0800

    optimize key mappings

--- lua/wizcas/vscode-keys.lua
+++ lua/wizcas/vscode-keys.lua
@@ -4,7 +4,9 @@ local keymap = vim.api.nvim_set_keymap
 local vscodeCmds = {
   save = 'workbench.action.files.save',
   close = 'workbench.action.closeActiveEditor',
-  revert = 'workbench.action.files.revert'
+  revert = 'workbench.action.files.revert',
+  showHover = 'editor.action.showHover',
+  paramHints = 'editor.action.triggerParameterHints',
 }
 
 local function getVsCodeCmd(cmd)
@@ -15,3 +17,5 @@ keymap("n", "<leader>w", getVsCodeCmd(vscodeCmds['save']), opts)
 keymap("n", "<leader>q", getVsCodeCmd(vscodeCmds['close']), opts)
 keymap("n", "<leader>E", getVsCodeCmd(vscodeCmds['revert']), opts)
 keymap("n", "<leader>h", ':noh<CR>', opts)
+keymap("n", "K", getVsCodeCmd(vscodeCmds['showHover']), opts)
+keymap("n", "gk", getVsCodeCmd(vscodeCmds['paramHints']), opts)
